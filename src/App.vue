d2cd7872940e16228cb13e8337544e1a7c128de9
zeroone001 <skylys54@gmail.com>
2021-10-15 15:15:30 +0800

    reactive

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <template>
   <div>
     <my-props :name="name"></my-props>
+    <div>{{ myShow.name }}</div>
   </div>
 </template>
 
 <script lang="ts">
-import { ref, defineComponent } from 'vue'
+import { ref, defineComponent, reactive, onMounted } from 'vue'
 import MyProps from './components/props.vue';
 export default defineComponent({
   components: {
@@ -13,7 +14,36 @@ export default defineComponent({
   },
   setup(props: any) {
     const name = ref('lys------>');
+    /* 
+    reactive不能直接赋值 而是
+    const obj = ref({
+      a:2
+    });
+    obj.value = {a: 123}
+    或者
+    const obj=reactive({
+    data: {
+        a:2
+      }
+    });
+    obj.data = {a: 312}
+    */
+    let myShow = ref({
+      name: 'lysssss'
+    });
+    console.log('myShowmyShow', myShow);
+    
+    onMounted(()=> {
+      setTimeout(()=> {
+        myShow.value = {
+          name: 'lysssss2'
+        }
+        console.log('myShow', myShow);
+        
+      }, 1000);
+    })
     return { 
+      myShow,
       name
      }
   }
